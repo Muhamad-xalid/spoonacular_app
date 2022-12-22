36343fd0255f132b1f14ff1d188c5cc402e08483
Muhamad-xalid <muhamadxalidahmad00@gmail.com>
2022-12-23 01:30:04 +0300

    add dark mode

--- src/Request.js
+++ src/Request.js
@@ -3,15 +3,24 @@ import axios from 'axios'
 import data from './ourproducts.json'
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from "react-icons/fa";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+
 export default function Request() {
     console.log(data.results);
+    const [darkmode,setdarkmode] = useState(false)
     const [recipes,setrecipes] = useState(data.results)
 
+    
 return (
-    <div className='container mx-auto my-10 '>
-        <div>
-        <Link to='/' className='text-xl text-red-900'><FaArrowLeft/></Link>
-            <h1 className='text-center mb-7 text-3xl text-red-900 font-black'>Our Products</h1>
+   <div className={`'m-0 p-0 ${darkmode && 'dark:bg-black'} '`}>
+     <div className='container mx-auto '>
+        <div className='pt-10'>
+            <div className='flex justify-between'>
+            <Link to='/' className='text-xl text-red-900 '><FaArrowLeft/></Link>
+            <button onClick={()=>setdarkmode(!darkmode)} className='text-2xl text-red-900 '>{!darkmode ?<MdOutlineDarkMode/> :<MdDarkMode/>}</button>
+            </div>
+            <h1 className='text-center mb-7 text-3xl text-red-900 font-black '>Our Products</h1>
         </div>
         <div className='grid grid-cols-1  md:grid-cols-2  lg:grid-cols-3 '>
         { recipes && recipes.map((recipe)=>{
@@ -24,6 +33,7 @@ return (
         })} 
         </div>
     </div>
+   </div>
   )
 }
 
